ad0604b6dd766ded9db1ea0946b0da08416222de
Admin <buibahuy250399@gmail.com>
2023-05-28 22:14:14 +0700

    update swipe item

--- app/src/main/java/com/techja/themoviekotlin/view/fragment/M002ListMovieFrg.kt
+++ app/src/main/java/com/techja/themoviekotlin/view/fragment/M002ListMovieFrg.kt
@@ -1,9 +1,12 @@
 package com.techja.themoviekotlin.view.fragment
 
+import android.graphics.*
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.techja.themoviekotlin.R
 import com.techja.themoviekotlin.api.res.model.MovieRes
 import com.techja.themoviekotlin.databinding.M002ListMovieFrgBinding
 import com.techja.themoviekotlin.view.adapter.MovieAdapter
@@ -45,8 +48,7 @@ class M002ListMovieFrg : BaseFragment<M002ListMovieFrgBinding, M002ListMovieVM>(
         }
         mAdapter.updateListResult(viewModel.getResultList())
 
-        val itemTouchHelper = ItemTouchHelper(object :
-            ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN,0){
+        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN , ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT){
             override fun onMove(
                 recyclerView: RecyclerView,
                 soure: RecyclerView.ViewHolder,
@@ -62,7 +64,45 @@ class M002ListMovieFrg : BaseFragment<M002ListMovieFrgBinding, M002ListMovieVM>(
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                TODO("Not yet implemented")
+                val position = viewHolder.adapterPosition
+                if (direction == ItemTouchHelper.LEFT) {
+                    //mAdapter.notifyItemChanged(position)
+                    Toast.makeText(requireContext(), "Swipe left", Toast.LENGTH_LONG).show()
+                } else if (direction == ItemTouchHelper.RIGHT) {
+//                    mAdapter.notifyItemChanged(position)
+                    Toast.makeText(requireContext(), "Swipe right", Toast.LENGTH_LONG).show()
+                }
+            }
+            override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float,
+                                     actionState: Int, isCurrentlyActive: Boolean) {
+                val p = Paint()
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                    val itemView = viewHolder.itemView
+                    val height = itemView.bottom.toFloat() - itemView.top.toFloat()
+                    val width = height / 3
+
+                    if (dX < 0) {
+                        //p.color = Color.RED
+                        val background = RectF(itemView.right.toFloat() + dX, itemView.top.toFloat(), itemView.right.toFloat(), itemView.bottom.toFloat())
+                        c.drawRect(background, p)
+                        val icon = BitmapFactory.decodeResource(mContext.resources, R.drawable.ic_heart)
+                        val margin = (dX / 5 - width) / 2
+                        val iconDest = RectF(itemView.right.toFloat() + margin, itemView.top.toFloat() + width, itemView.right.toFloat() + (margin + width), itemView.bottom.toFloat() - width)
+                        c.drawBitmap(icon, null, iconDest, p)
+                    }
+                    if (dX > 0) {
+                        p.color = Color.BLUE
+                        val background = RectF(itemView.left.toFloat(), itemView.top.toFloat(), itemView.left.toFloat() + dX, itemView.bottom.toFloat())
+                        c.drawRect(background, p)
+                        val icon = BitmapFactory.decodeResource(mContext.resources, R.drawable.ic_book_mark)
+                        val margin = (dX / 5 - width) / 2
+                        val iconDest = RectF(margin, itemView.top.toFloat() + width, margin + width, itemView.bottom.toFloat() - width)
+                        c.drawBitmap(icon, null, iconDest, p)
+                    }
+                } else {
+                    c.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX / 5, dY, actionState, isCurrentlyActive)
             }
         })
 
